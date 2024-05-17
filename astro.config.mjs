a670dcb2a411d4be15afbef6190c8d4adcea30fd
Aayan Mateen <56849865+aayanmtn@users.noreply.github.com>
2024-05-17 13:20:15 +0530

    Update astro.config.mjs analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,15 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+
 export default defineConfig({
   site: "https://astroship.web3templates.com",
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [tailwind(), mdx(), sitemap()],
 });
